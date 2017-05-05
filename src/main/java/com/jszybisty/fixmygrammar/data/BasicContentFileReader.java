19876ff6d0beee0892b3347b7f2119fddfe06a8a
grizzlyJoe <kubasz3@wp.pl>
2017-05-05 23:44:57 +0200

    Changed way of retrieving files

--- src/main/java/com/jszybisty/fixmygrammar/data/BasicContentFileReader.java
+++ src/main/java/com/jszybisty/fixmygrammar/data/BasicContentFileReader.java
@@ -1,16 +1,15 @@
 package com.jszybisty.fixmygrammar.data;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Collectors;
 //
+
 /**
  * Created by jakub on 17.04.2017.
  */
@@ -18,12 +17,8 @@ import java.util.List;
 public class BasicContentFileReader {
 
     public List<String> readContentFromFile(String fileName) {
-        try {
-            URI uri = new ClassPathResource(fileName).getURI();
-            Path dataFile = Paths.get(uri);
-            return new ArrayList<>(Files.readAllLines(dataFile));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+        return new BufferedReader(new InputStreamReader(resourceAsStream,
+                StandardCharsets.UTF_8)).lines().collect(Collectors.toList());
     }
 }
